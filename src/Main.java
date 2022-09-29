4418726b4f1010b9ddaa8ce614cfac792128a8ce
100133917 <100133917@lubbockisd.net>
2022-09-29 08:46:26 -0500

    added math class

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,24 @@ public class Main {
 
         int ans = dudeTheThird.multiplier(7, 12);
         out.println("the answer is " + ans);
+
+
+        //anytime you need to use a method from the Math clas
+        //just start Math. and let the list pop up
+        //Math.
+
+
+        //random numbers (rng)
+        //pseudorandom numbers (png)
+
+
+        //Math.random(); == 0.0 up to but not including 1
+
+
+        // (int)(Math.random() * [range] + lower)
+        //                upper - lower + 1
+        int value = (int)(Math.random()*20 +1);
+
+        out.println("you rolled: " + value);
     }
 }
